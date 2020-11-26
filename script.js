3c738a70e5b33fe3a04ffc34664d69cf61f5b286
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-11-26 18:01:45 -0500

    updated clear all button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 //API for Taco
-// http://taco-randomizer.herokuapp.com/random/?full-taco=true
+// https://taco-randomizer.herokuapp.com/random/?full-taco=true
 
 document.getElementById('buttonCombo').onclick = function getTacoAndBeer() {
 	
@@ -13,49 +13,51 @@ document.getElementById('buttonCombo').onclick = function getTacoAndBeer() {
 //complete data stream		
 	const data = await response.json();
 	const {tacoName} = data;
+//test in console	
+	console.log(data.base_layer.name)	
 //display in browser as text		
 	document.getElementById("viewTaco").innerHTML = ("Taco: " + data.base_layer.name);
-//	document.getElementById("FavoriteTaco").innerHTML =("Favorite Taco: " + data.base_layer.name);	
 
-document.getElementById("saveFaves").addEventListener("click", myFavesTaco);
-
-function myFavesTaco() {
-	console.log(data.base_layer.name);
-	document.getElementById("FavoriteTaco").innerHTML =("Favorite Taco: " + data.base_layer.name);
-	
-}
-	
+//onclick event for add to favorites button
+document.getElementById('saveTacoFaves').onclick = function myFaveTaco() {
+// autopopulate favorite Taco from API data	
+	document.getElementById("FavoriteTaco").innerHTML = ("Favorite Taco: " + data.base_layer.name);
+}		
 	}
 
 getTaco();
 
 
+
 //API for Beer
-// https://api.punkapi.com/v2/beers/random
+// http://https://api.punkapi.com/v2/beers/random
 
 //call fetch
 fetch("https://api.punkapi.com/v2/beers/random")
     .then(response => response.json())
     .then(beers => {
 	const beer = beers[0].name;
+	//test in console
+	console.log(beer)
 	//display in browser as text
 	document.getElementById("viewBeer").innerHTML = ("Beer: " + beer);
-//	document.getElementById("FavoriteBeer").innerHTML =("Favorite Beer: " + beer);
 	
-	document.getElementById("saveFaves").addEventListener("click", myFavesBeer);
-
-function myFavesBeer() {
-	console.log(beer);
-	document.getElementById("FavoriteBeer").innerHTML =("Favorite Beer: " + beer);	
-	
+	//onclick event for add to favorites button
+document.getElementById('saveBeerFaves').onclick = function myFaveBeer() {
+// autopopulate favorite Beer from API data	
+	document.getElementById("FavoriteBeer").innerHTML = ("Favorite Beer: " + beer);
 }
-	
+
 })
 	
-}
-	
-	
+	}
 
+//Show me what is in local storage
+console.log(localStorage)
+//Clear Local Storage
+document.getElementById('clear').onclick = function myFaveBeer() {
+	window.localStorage.clear();
+}
 
 
 
