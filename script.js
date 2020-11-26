8280b6f75125747338f1f5218defacddc57a8391
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-11-26 07:57:47 -0500

    created auto populate for add to favorites button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 document.getElementById('buttonCombo').onclick = function getTacoAndBeer() {
 	
 //call fetch
-	var api_url = "http://taco-randomizer.herokuapp.com/random/?full-taco=true";
+	var api_url = "https://taco-randomizer.herokuapp.com/random/?full-taco=true";
 	async function getTaco (){
 //response text
 	const response = await fetch(api_url);
@@ -15,48 +15,48 @@ document.getElementById('buttonCombo').onclick = function getTacoAndBeer() {
 	const {tacoName} = data;
 //display in browser as text		
 	document.getElementById("viewTaco").innerHTML = ("Taco: " + data.base_layer.name);
-	document.getElementById("FavoriteTaco").innerHTML =("Favorite Taco: " + data.base_layer.name);	
+//	document.getElementById("FavoriteTaco").innerHTML =("Favorite Taco: " + data.base_layer.name);	
+
+document.getElementById("saveFaves").addEventListener("click", myFavesTaco);
+
+function myFavesTaco() {
+	console.log(data.base_layer.name);
+	document.getElementById("FavoriteTaco").innerHTML =("Favorite Taco: " + data.base_layer.name);
+	
+}
+	
 	}
 
 getTaco();
 
 
 //API for Beer
-// http://https://api.punkapi.com/v2/beers/random
+// https://api.punkapi.com/v2/beers/random
 
 //call fetch
 fetch("https://api.punkapi.com/v2/beers/random")
     .then(response => response.json())
     .then(beers => {
 	const beer = beers[0].name;
-	//test in console
-	console.log(beer)
 	//display in browser as text
 	document.getElementById("viewBeer").innerHTML = ("Beer: " + beer);
-	document.getElementById("FavoriteBeer").innerHTML =("Favorite Beer: " + beer);
+//	document.getElementById("FavoriteBeer").innerHTML =("Favorite Beer: " + beer);
+	
+	document.getElementById("saveFaves").addEventListener("click", myFavesBeer);
+
+function myFavesBeer() {
+	console.log(beer);
+	document.getElementById("FavoriteBeer").innerHTML =("Favorite Beer: " + beer);	
+	
+}
+	
 })
-
-
 	
-//	document.getElementById('saveFaves').onclick = function saveCombo(){
-		
-//	}
+}
 	
-	}
-
-//Save Favorites
-
-//var myFaves = document.querySelector('#Favorites');
-//retrieves api data
-//myFaves.value = localStorage.getItem("beer", "data.base_layer.name");
-//when the Save button is clicked, store the api data as favorites
-//myFaves.addEventListener('click', saveFaves);
-//sets the input of text area into local storage
-//function saveFaves() {
-  //  localStorage.setItem('Favorites', myFaves.value);
 	
-//	console.log(localStorage);
-//}
+
+
 
 
 
